776e571f3e8a82d0bdcf392c039e1a85249d194d
Kirill <kirillgel1os@gmail.com>
2015-11-21 19:54:52 +0100

    code refactoring

--- main.js
+++ main.js
@@ -1,210 +1,383 @@
-var settings = {
+;
+(function() {
+    var settings = {
         'gridSize': 20,
         'startDirection': 'right',
         'speedOfMovement': 200,
         'minimalSpeed': 100,
-        'increaseAfter': 50,
-        'reduceBy': 20
-    },
-    currentSnakesState = [],
-    gameOver = false,
-    paused = false,
-    resumingGame = false;
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function generateGrid(gridSize) {
-    var arr = Array.apply(null, {
-            length: gridSize
-        }).map(Number.call, Number),
-        firstPoint,
-        snakesHead,
-        snakesTail;
-    $('.wrapper').append($('<div class="grid-container"></div>'));
-    arr.forEach(function(n, i) {
-        $('.grid-container').append($('<div class="row r' + n + '"></div>'));
-        arr.forEach(function(_n) {
-            $('.row.r' + n).append($('<div class="cell c' + _n + ' r' + n + '" data-row="' + n + '" data-cell="' + _n + '"></div>'));
-        });
-    });
-
-    firstPoint = '.grid-container .r' + getRandomInt(0, gridSize - 1) + '.c' + getRandomInt(0, gridSize - 1);
-    snakesHead = '.grid-container .r10 .c10';
-    snakesTail = '.grid-container .r10 .c9, .grid-container .r10 .c8';
-
-    // @ currentSnakesState[row, cell]
-    currentSnakesState = [
-        [10, 8],
-        [10, 9],
-        [10, 10]
-    ];
-
-    $(firstPoint).addClass('black');
-    $(snakesHead).addClass('snake snakesHead');
-    $(snakesTail).addClass('snake');
-    return gridSize;
-}
-
-function addBlackCell() {
-    var emptyCells = '.cell:not(.snake):not(.black)';
-    $($(emptyCells)[Math.floor(Math.random() * $(emptyCells).length)]).addClass('black');
-}
-
-function finishGame(interval) {
-    clearInterval(interval);
-    $('.gameOver .scoreMes').text('Your score is: ' + jQuery('.totalScore').text());
-    $('.gameOver').removeClass('hidden');
-    gameOver = true;
-}
-
-function tryAgain() {
-    location.reload();
-}
-
-function countScore() {
-    var $totalScore = $('.totalScore');
-    $totalScore.text(+$totalScore.text() + 10);
-
-    if (+$totalScore.text() % settings['increaseAfter'] === 0 && settings['speedOfMovement'] !== settings['minimalSpeed']) {
-        $(document).trigger('increaseSpeed')
-    }
-}
-
-function moveSnake(direction, size, interval) {
+        'increaseSpeedEvery': 50,
+        'increaseSpeedBy': 20
+    };
     var directions = {
-            //description:
-            //direction[0] === cells,
-            //direction[1] === rows,
-            //direction[2] === limited by,
-            //directions[3] === limit value
-            top: [0, -1, 'data-row', 0],
-            right: [1, 0, 'data-cell', size - 1],
-            bottom: [0, 1, 'data-row', size - 1],
-            left: [-1, 0, 'data-cell', 0]
+        top: {
+            'nextCell': 0,
+            'nextRow': -1,
+            'limitation': 'data-row',
+            'limitationValue': 0
         },
+        bottom: {
+            'nextCell': 0,
+            'nextRow': 1,
+            'limitation': 'data-row',
+            'limitationValue': settings.gridSize - 1
+        },
+        right: {
+            'nextCell': 1,
+            'nextRow': 0,
+            'limitation': 'data-cell',
+            'limitationValue': settings.gridSize - 1
+        },
+        left: {
+            'nextCell': -1,
+            'nextRow': 0,
+            'limitation': 'data-cell',
+            'limitationValue': 0
+        }
+    };
+    var directionKeyCodes = {
+        '37': {
+            'direction': 'left',
+            'opposite': 'right'
+        },
+        '65': {
+            'direction': 'left',
+            'opposite': 'right'
+        },
+        '87': {
+            'direction': 'top',
+            'opposite': 'bottom'
+        },
+        '38': {
+            'direction': 'top',
+            'opposite': 'bottom'
+        },
+        '39': {
+            'direction': 'right',
+            'opposite': 'left'
+        },
+        '68': {
+            'direction': 'right',
+            'opposite': 'left'
+        },
+        '40': {
+            'direction': 'bottom',
+            'opposite': 'top'
+        },
+        '83': {
+            'direction': 'bottom',
+            'opposite': 'top'
+        }
+    };
+    var currentSnakesState = [];
+    var gameOver = false;
+    var paused = false;
+    var resumingGame = false;
+    var $doc = $(document);
+    var direction = settings.startDirection;
+    var movement;
+    var keyPressed = false;
+    var gridSize;
 
-        $snakesHead = $('.snakesHead'),
-        currentRow = $snakesHead.attr('data-row'),
-        currentCell = $snakesHead.attr('data-cell'),
-        nextCell,
-        nextRow,
-        nextElem;
-
-    // @ currentSnakesState[row, cell]
-    nextCell = +currentCell + directions[direction][0];
-    nextRow = +currentRow + directions[direction][1];
-    nextElem = $('.grid-container .r' + nextRow + '.c' + nextCell);
-
-    currentSnakesState.push([nextRow, nextCell]);
-
-    if (nextElem.hasClass('snake')) {
-        finishGame(interval, 'You lose, sucker!!');
-        return;
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    if (nextElem.hasClass('black')) {
-        $snakesHead.removeClass('snakesHead');
-        nextElem.removeClass('black').addClass('snake snakesHead');
-        //moveHead(direction, size, interval);
-        countScore();
-        addBlackCell();
-    } else {
-        nextElem.addClass('snake snakesHead');
-        if (+$snakesHead.attr(directions[direction][2]) === directions[direction][3]) {
-            finishGame(interval, 'You lose, sucker!!');
+    /**
+     * create array of elements from 0 to elemNumber
+     * @param elemNumber
+     * @returns {Array|*}
+     */
+    function createArray(elemNumber) {
+        return Array.apply(null, {length: elemNumber}).map(Number.call, Number);
+    }
+
+    function createRow(rowNumber) {
+        return $('<div/>', {
+            'class': 'row  r' + rowNumber
+        });
+    }
+
+    function createCell(rowNumber, cellNumber) {
+        return $('<div/>', {
+            'class': 'cell r' + rowNumber + ' c' + cellNumber,
+            'data-row': rowNumber,
+            'data-cell': cellNumber
+        });
+    }
+
+    function generateGrid(gridSize) {
+        var arr = createArray(gridSize);
+        var $gridContainer = $('.grid-container');
+        var $row;
+        var $cell;
+
+        arr.forEach(function(rowNumber) {
+            $row = createRow(rowNumber);
+
+            arr.forEach(function(cellNumber) {
+                $cell = createCell(rowNumber, cellNumber);
+                $row.append($cell);
+            });
+
+            $gridContainer.append($row);
+        });
+
+        return gridSize;
+    }
+
+    /**
+     * set initial snake's position
+     */
+    function placeSnake() {
+        var $snakesHead = $('.r10 .c10');
+        var $snakesTail = $('.r10 .c9, .r10 .c8');
+
+        currentSnakesState = [[10, 8], [10, 9], [10, 10]];
+
+        $snakesHead.addClass('snake snake-head');
+        $snakesTail.addClass('snake');
+    }
+
+    /**
+     * set initial position of point
+     * @param gridSize - number
+     */
+    function placeFirstBlackCell() {
+        var randomRow = getRandomInt(0, gridSize - 1);
+        var randomCell = getRandomInt(0, gridSize - 1);
+        var $firstPoint = $('.r' + randomRow + '.c' + randomCell);
+        $firstPoint.addClass('black');
+    }
+
+    function addBlackCell() {
+        var $emptyCells = $('.cell:not(.snake):not(.black)');
+        var emptyCellsAmount = $emptyCells.length;
+        var randomCellIndex = Math.floor(Math.random() * emptyCellsAmount);
+        var $randomCell = $($emptyCells[randomCellIndex]);
+
+        $randomCell.addClass('black')
+    }
+
+    function increaseSnake(head, blackCell) {
+        head.removeClass('snake-head');
+        blackCell.removeClass('black').addClass('snake snake-head');
+    }
+
+    function finishGame() {
+        var $gameOverDiv = $('.gameOver');
+        var $scoreMessage = $gameOverDiv.find('.score-message');
+        var totalScore = $('.total-score').text();
+
+        $scoreMessage.text('Your score is: ' + totalScore);
+        $gameOverDiv.removeClass('hidden');
+        gameOver = true;
+    }
+
+    function tryAgain() {
+        location.reload();
+    }
+
+    function countScore() {
+        var $totalScore = $('.total-score');
+        var score = parseInt($totalScore.text(), 10);
+
+        $totalScore.text(score + 10);
+
+        if (score % settings.increaseSpeedEvery === 0) {
+            $doc.trigger('increaseSpeed');
+        }
+    }
+
+    /**
+     * check if snake's head reached border of grid
+     * @param head - DOM element
+     * @param direction - object
+     */
+    function isReachedBorder(head, direction) {
+        var limitationAxis = direction.limitation;
+        var currentLimitationValue = parseInt(head.attr(limitationAxis), 10);
+        return currentLimitationValue === direction.limitationValue;
+    }
+
+    /**
+     * set nextCell as a head of snake,
+     * shift snake according to current snake's state
+     */
+    function shiftSnake(nextCell) {
+
+        var lastTailElementCoordinates = currentSnakesState[0];
+        var lastRow = lastTailElementCoordinates[0];
+        var lastCell = lastTailElementCoordinates[1];
+
+        var $lastTailElement = $('.r' + lastRow + '.c' + lastCell);
+        var $snakeHead = $('.snake-head');
+
+        $snakeHead.removeClass('snake-head');
+        nextCell.addClass('snake snake-head');
+
+        $lastTailElement.removeClass('snake');
+
+        currentSnakesState.splice(0, 1);
+    }
+
+    /**
+     * get next cell according to movement direction
+     * @param head - DOM element
+     * @param direction - object
+     * @returns {*|jQuery|HTMLElement} - DOM element of next cell
+     */
+    function countNextCell(head, direction) {
+
+        var row = parseInt(head.attr('data-row'), 10);
+        var cell = parseInt(head.attr('data-cell'), 10);
+        var nextCell;
+        var nextRow;
+
+        nextCell = cell + direction.nextCell;
+        nextRow = row + direction.nextRow;
+
+        currentSnakesState.push([nextRow, nextCell]);
+
+        return $('.r' + nextRow + '.c' + nextCell);
+    }
+
+    function moveSnake(head, nextCell, direction) {
+        var isBorder = isReachedBorder(head, direction);
+
+        if (isBorder) {
+            finishGame('You lose, sucker!!');
+        } else {
+            shiftSnake(nextCell);
+        }
+    }
+
+    function initSnakeMovement(direction) {
+        direction = directions[direction];
+
+        var $snakesHead = $('.snake-head');
+        var $nextCell = countNextCell($snakesHead, direction);
+
+        if ($nextCell.hasClass('snake')) {
+            finishGame('You lose, sucker!!');
+        } else if ($nextCell.hasClass('black')) {
+            increaseSnake($snakesHead, $nextCell);
+            countScore();
+            addBlackCell();
+        } else {
+            moveSnake($snakesHead, $nextCell, direction);
+        }
+    }
+
+    function changeDirection(chosenDirection) {
+        keyPressed = true;
+        direction = chosenDirection;
+        setTimeout(function() {
+            keyPressed = false;
+        }, settings.speedOfMovement);
+    }
+
+    function pauseGame(popup) {
+        clearInterval(movement);
+        paused = true;
+        popup.removeClass('hidden');
+    }
+
+    function restoreInitialResumeMessage(message) {
+        var initialHtml = '<span>Game paused.</span>' +
+            '<br>' +
+            '<span>Hit space to continue...</span>';
+        message
+            .removeClass('countdown')
+            .html(initialHtml);
+    }
+
+    function resumeGame($popup, $message) {
+        resumingGame = true;
+
+        $message.text('|||');
+        $message.addClass('countdown');
+
+        var showCountdown = setInterval(function() {
+            $message.text($message.text().replace('|', ''))
+        }, 500);
+
+        setTimeout(function() {
+            clearInterval(showCountdown);
+            $popup.addClass('hidden');
+            restoreInitialResumeMessage($message);
+            paused = false;
+            initMovementInsideInterval();
+            resumingGame = false;
+        }, 1500);
+    }
+
+    function togglePausing() {
+        var $pausedPopUp = $('.paused');
+        var $pausedMsg = $('.pause-message');
+
+        if (paused) {
+            resumeGame($pausedPopUp, $pausedMsg);
+        } else {
+            pauseGame($pausedPopUp);
+        }
+    }
+
+    /**
+     * executed after "keyup" event is fired
+     *
+     */
+    function keyUpHandler(event) {
+
+        var pressedCode = event.keyCode;
+        var ENTER_CODE = 13;
+        var SPACE_CODE = 32;
+        var chosenDirection = directionKeyCodes[pressedCode];
+        var isOppositeDirection;
+
+        if (keyPressed) {
             return;
         }
 
-        for (var i = currentSnakesState.length - 1; i > 0; i--) {
-            $('.grid-container .r' + currentSnakesState[i - 1][0] + '.c' + currentSnakesState[i - 1][1]).removeClass('snake snakesHead');
-            $('.grid-container .r' + currentSnakesState[i][0] + '.c' + currentSnakesState[i][1]).addClass('snake');
-        }
-        currentSnakesState.splice(0, 1);
-    }
-}
-
-$(document).ready(function() {
-    var gridSize = generateGrid(settings['gridSize']),
-        movement,
-        direction = settings['startDirection'],
-        keyPressed = false;
-
-    movement = setInterval(function() {
-        moveSnake(direction, gridSize, movement);
-    }, settings['speedOfMovement']);
-
-    $(document).on('increaseSpeed', function() {
-        settings['speedOfMovement'] -= settings['reduceBy'];
-        clearInterval(movement);
-        movement = setInterval(function() {
-            moveSnake(direction, gridSize, movement);
-        }, settings['speedOfMovement']);
-    });
-
-    $(document).keyup(function(event) {
-
-        var keyCodes = {
-            // сode : [moveTo, opposite]
-            '37': ['left', 'right'],
-            '65': ['left', 'right'],
-            '87': ['top', 'bottom'],
-            '38': ['top', 'bottom'],
-            '39': ['right', 'left'],
-            '68': ['right', 'left'],
-            '40': ['bottom', 'top'],
-            '83': ['bottom', 'top']
-        };
-
-        if (keyPressed) {
-            return
-        }
-
-        if (keyCodes[event.keyCode] && direction !== keyCodes[event.keyCode][1]) {
-            keyPressed = true;
-            direction = keyCodes[event.keyCode][0];
-            setTimeout(function() {
-                keyPressed = false;
-            }, settings['speedOfMovement']);
-        } else if (event.keyCode === 13) {
-            tryAgain();
-        } else if (event.keyCode === 32 && !gameOver && !resumingGame) {
-            var $pausedPopUp = $('.paused'),
-                $pausedMsg = $('.pauseMsg');
-
-            if (paused) {
-                resumingGame = true;
-
-                $pausedMsg.text('|||');
-                $pausedMsg.addClass('countdown');
-
-                var showCountdown = setInterval(function() {
-                    $pausedMsg.text($pausedMsg.text().replace('|', ''))
-                }, 500);
-
-                // start countdown for resuming game
-                setTimeout(function() {
-                    clearInterval(showCountdown);
-                    $pausedPopUp.addClass('hidden');
-                    $pausedMsg.removeClass('countdown');
-                    $pausedMsg.html('<span>Game paused.</span><br><span>Hit space to continue...</span>');
-                    paused = false;
-                    movement = setInterval(function() {
-                        moveSnake(direction, gridSize, movement);
-                    }, settings['speedOfMovement']);
-                    resumingGame = false;
-                }, 1500);
-
-            } else {
-                // pause game
-                clearInterval(movement);
-                paused = true;
-                $pausedPopUp.removeClass('hidden');
+        if (chosenDirection) {
+            isOppositeDirection = direction === chosenDirection.opposite;
+            if (!isOppositeDirection) {
+                changeDirection(chosenDirection.direction);
             }
+        } else if (event.keyCode === ENTER_CODE) {
+            tryAgain();
+        } else if (event.keyCode === SPACE_CODE && !gameOver && !resumingGame) {
+            togglePausing();
         }
-    });
-    $('.tryAgain').click(function() {
-        tryAgain();
-    })
-});
+    }
+
+    function initMovementInsideInterval() {
+        movement = setInterval(function() {
+            initSnakeMovement(direction, movement);
+            if (gameOver) {
+                clearInterval(movement);
+            }
+        }, settings.speedOfMovement);
+    }
+
+    function increaseSpeed() {
+        settings.speedOfMovement -= settings.increaseSpeedBy;
+
+        if (settings.speedOfMovement >= 0) {
+            clearInterval(movement);
+            initMovementInsideInterval()
+        }
+    }
+
+    function init() {
+        gridSize = generateGrid(settings['gridSize']);
+        placeSnake();
+        placeFirstBlackCell();
+        initMovementInsideInterval();
+
+        $doc.on('increaseSpeed', increaseSpeed);
+        $doc.on('keyup', keyUpHandler);
+        $('.try-again').click(tryAgain);
+    }
+
+    $(init);
+}());
